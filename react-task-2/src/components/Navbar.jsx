8baf0f63e56dc020cd81d61a7a997db244fd95f1
nithya <nithukitty112@gmail.com>
2025-04-15 09:50:03 +0530

    update

--- react-task-2/src/components/Navbar.jsx
+++ react-task-2/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar( {count}) {
   return (
   <>
     <div className='fixed z-[50] w-full px-4 py-4  md:px-6 bg-blue-400 flex justify-between '>
-    <div className='text-2xl bold '>Shopping🛒 </div>
+    <div className='text-2xl bold '>Shopping 🛒 </div>
     <div>
       <div className='flex relative z-[20]'>
     <div className='text-2xl md:text-3xl md:mr-6 bold hover:text-red-600'><Link to="/cart">Cart </Link></div>
